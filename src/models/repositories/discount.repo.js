6841d2766a8626a540cac3ea2695cf68d4299d64
Pham Duy Hoang <97928971+kng-duyhoang@users.noreply.github.com>
2024-10-23 10:52:46 +0700

    update

--- src/models/repositories/discount.repo.js
+++ src/models/repositories/discount.repo.js
@@ -39,7 +39,6 @@ const getAllDiscountSelectCodeByShop = async ({
 }
 
 const checkDiscountExist = async (model, filter) => {
-    console.log(filter);
     return await model.findOne(filter).lean()
 }
 
